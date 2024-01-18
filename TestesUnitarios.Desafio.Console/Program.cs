b48d5c681e2035641bfdf5ac30a7264287b86146
jucielefernandes <juciele.bol@gmail.com>
2024-01-18 18:24:59 -0300

    TestarDesempenhoMultiplicacao

--- TestesUnitarios.Desafio.Console/Program.cs
+++ TestesUnitarios.Desafio.Console/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace TestesUnitarios.Desafio.Console.Tests
+{
+    
+    public class ValidacoesListaTests
+    {
+        
+        public void TestarDesempenhoMultiplicacaoNumerosLista()
+        {
+            
+            ValidacoesLista validacoesLista = new ValidacoesLista();
+            List<int> listaGrande = new List<int>();
+
+            
+            for (int i = 0; i < 1000000; i++)
+            {
+                listaGrande.Add(i);
+            }
+
+          
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            List<int> resultado = validacoesLista.MultiplicarNumerosLista(listaGrande, 2);
+            stopwatch.Stop();
+
+            
+            Assert.Less(stopwatch.ElapsedMilliseconds, 100, "O teste excedeu o limite de tempo permitido.");
+        }
+    }
+// legal
